c55ba0fa83550fe202c61f9aad1d55c2e5678cb0
BradDWagner <110359842+BradDWagner@users.noreply.github.com>
2023-01-15 18:58:06 -0800

    tweaks to contact styling

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -27,8 +27,12 @@ export default function Contact() {
         const inputType = target.name;
         const inputValue = target.value;
 
-        if (inputValue.length < 1) {
+        if (inputType === 'Name' || inputType === 'Message' && inputValue.length < 1) {
             setErrorMessage(`${inputType} is required`);
+            return;
+        } else if (!validateEmail(inputValue)) {
+            setErrorMessage('Please enter a valid email');
+            return;
         } else {
             setErrorMessage('');
         }
